c0a517b1dcc6534545d28e15117715378d8d74dc
和田 充弘 <benzoh.wada@gmail.com>
2020-05-24 21:07:24 +0900

    Change: SectionList

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { SectionList, View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 44,
+  },
+  label: {
+    color: 'black',
+    backgroundColor: 'white'
+  },
+  sectionHeader: {
+    color: 'white',
+    backgroundColor: 'black'
+  }
+});
+
+const sections = [
+  { title: 'basic', data: ['View', 'Text', 'Image'] },
+  { title: 'list', data: ['FlatList', 'SectionList'] },
+  { title: 'interaction', data: ['Touchable*', 'TextInput'] },
+];
+
+export default function App() {
+  return (
+    <SectionList
+      style={styles.container}
+      sections={sections}
+      renderItem={({ item }) => <Text style={styles.label}>{item}</Text>}
+      keyExtractor={(_, index) => index.toString()}
+      renderSectionHeader={({ section: { title } }) => <Text style={styles.sectionHeader}>{title}</Text>}
+    />
+  );
+}
+
