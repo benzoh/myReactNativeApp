b84d95af96e7c74df7651e8172ec9836951eb6ff
和田 充弘 <benzoh.wada@gmail.com>
2020-05-29 20:55:13 +0900

    ## Clipboard

--- App.tsx
+++ App.tsx
@@ -1,19 +1,43 @@
 import React from 'react';
-import { BackHandler, View, Text } from 'react-native';
+import { AppState, Clipboard, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
 
 export default function App() {
+  const [content, setContent] = React.useState('');
   React.useEffect(() => {
-    const BackHandler = BackHandler.addEventListener('hardwareBackPress', () => {
-      console.warn('hardware back passed');
-      return true;
-    });
+    function getContentOfClipboard() {
+      Clipboard.getString().then(newContent => {
+        setContent(newContent);
+      });
+    }
+
+    getContentOfClipboard();
+    AppState.addEventListener('change', getContentOfClipboard);
 
     return () => {
-      BackHandler.remove();
+      AppState.removeEventListener('change', getContentOfClipboard);
     };
   }, []);
 
   return (
-    <View />
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          const newContent = 'present!!';
+          Clipboard.setString(newContent);
+          setContent(newContent);
+        }}
+      >
+        <Text>set clipboard</Text>
+      </TouchableOpacity>
+      <Text>{content}</Text>
+    </View>
   );
 }
