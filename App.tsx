91ef9b792d856cd10cbf51c9ac630738bc68b369
和田 充弘 <benzoh.wada@gmail.com>
2020-05-31 08:48:50 +0900

    ## Share

--- App.tsx
+++ App.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
-import { Alert, PermissionsAndroid, View } from 'react-native';
+import { Share, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-async function requestCameraPermission() {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+async function onShare() {
   try {
-    const isGranted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA);
-    console.log(isGranted);
-
-    if (isGranted === PermissionsAndroid.RESULTS.GRANTED) {
-      Alert.alert('許可ありがとうございます');
-    } else {
-      Alert.alert('カメラを使うと便利ですよ！');
-    }
-  } catch (err) {
-    console.warn(err);
+    await Share.share({
+      title: 'タイトル',
+      message: '共有したいメッセージ',
+    })
+  } catch (e) {
+    console.log(e);
   }
 }
 
 export default function App() {
-  React.useEffect(() => {
-    requestCameraPermission();
-  });
-
   return (
-    <View />
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onShare}>
+        <Text>Share</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
