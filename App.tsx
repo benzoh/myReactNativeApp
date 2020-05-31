97b63dd978d658023041b00f20877e775658ff7a
和田 充弘 <benzoh.wada@gmail.com>
2020-05-31 21:22:15 +0900

    ## AsyncStorageモジュール

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, Vibration } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const styles = StyleSheet.create({
   container: {
@@ -9,16 +10,53 @@ const styles = StyleSheet.create({
   }
 });
 
+interface Data {
+  count: number;
+}
+
+async function hydrate() {
+  try {
+    const data = await AsyncStorage.getItem('keyForData');
+
+    if (data === null) {
+      return { count: 0 };
+    }
+    return JSON.parse(data);
+  } catch {
+    return { count: 0 };
+  }
+}
+
+async function persist(data: Date) {
+  await AsyncStorage.setItem('keyForData', JSON.stringify(data));
+}
+
 export default function App() {
+  const [count, setCount] = React.useState<number | null>(null);
+
+  async function loadCount() {
+    const data = await hydrate();
+    const newCount = data.count + 1;
+    setCount(newCount);
+    persist({ count: newCount });
+  }
+
+  React.useEffect(() => {
+    loadCount();
+  }, []);
+
+  const onPress = React.useCallback(async () => {
+    AsyncStorage.clear();
+    loadCount();
+  }, []);
+
+  if (count === null) {
+    return <View />;
+  }
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        onPress={() => {
-          Vibration.vibrate(1000)
-        }}
-      >
-        <Text>Vibrate</Text>
-      </TouchableOpacity>
+      <Text onPress={onPress}>{count}</Text>
     </View>
   );
 }
