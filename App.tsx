278438c953cc69b9351a3da46e9890c540523237
和田 充弘 <benzoh.wada@gmail.com>
2020-06-01 21:02:29 +0900

    ## SafeAreaViewコンポーネント

--- App.tsx
+++ App.tsx
@@ -1,62 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-});
-
-interface Data {
-  count: number;
-}
-
-async function hydrate() {
-  try {
-    const data = await AsyncStorage.getItem('keyForData');
-
-    if (data === null) {
-      return { count: 0 };
-    }
-    return JSON.parse(data);
-  } catch {
-    return { count: 0 };
-  }
-}
-
-async function persist(data: Date) {
-  await AsyncStorage.setItem('keyForData', JSON.stringify(data));
-}
+import { Text } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import SafeAreaView from 'react-native-safe-area-view';
 
 export default function App() {
-  const [count, setCount] = React.useState<number | null>(null);
-
-  async function loadCount() {
-    const data = await hydrate();
-    const newCount = data.count + 1;
-    setCount(newCount);
-    persist({ count: newCount });
-  }
-
-  React.useEffect(() => {
-    loadCount();
-  }, []);
-
-  const onPress = React.useCallback(async () => {
-    AsyncStorage.clear();
-    loadCount();
-  }, []);
-
-  if (count === null) {
-    return <View />;
-  }
-
   return (
-    <View style={styles.container}>
-      <Text onPress={onPress}>{count}</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <Text>見えやすい文字列</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
