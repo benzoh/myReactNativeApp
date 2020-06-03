39cbfc842ce69aa4c77b4a27cf761c143b44e300
和田 充弘 <benzoh.wada@gmail.com>
2020-06-03 21:26:18 +0900

    ### useCallback関数

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { ProgressBar } from '@react-native-community/progress-bar-android';
 
 const styles = StyleSheet.create({
   container: {
@@ -8,25 +7,23 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  wrapper: {
-    width: 256
-  }
 });
 
 export default function App() {
+  const [count1, setCount1] = React.useState(0);
+  const countUp1 = React.useCallback(() => {
+    setCount1(count1 + 1);
+  }, [count1]);
+
+  const [count2, setCount2] = React.useState(0);
+  const countUp2 = React.useCallback(() => {
+    setCount1(count2 + 1);
+  }, [count2]);
+
   return (
     <View style={styles.container}>
-      <ProgressBar />
-      <View style={styles.wrapper}>
-        <ProgressBar styleAttr="Horizontal" />
-      </View>
-      <View style={styles.wrapper}>
-        <ProgressBar
-          styleAttr="Horizontal"
-          indeterminate={false}
-          progress={0.37}
-        />
-      </View>
+      <Text onPress={() => { setCount1(count1 + 1) }}>{count1}</Text>
+      <Text onPress={() => { setCount2(count2 + 1) }}>{count2}</Text>
     </View>
   );
 }
