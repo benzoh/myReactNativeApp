7799022ee018c4a0170254c9e60aa18b6c2b74bc
和田 充弘 <benzoh.wada@gmail.com>
2020-06-06 20:39:58 +0900

    ## React Nativeにおけるアニメーション実装

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { TouchableOpacity, Platform, Text, View, StyleSheet, ViewStyle } from 'react-native';
-
-import Progress from './Progress';
+import { Animated, Dimensions, View, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -9,26 +7,27 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  progress: {
-    width: 256,
-  },
-  button: {
-    marginTop: 8
+  block: {
+    backgroundColor: 'black',
+    height: 32
   }
 });
 
+const MSEC_IN_FRAME = 1000 / 60;
+
 export default function App() {
-  const [progress, setProgress] = React.useState(Math.random());
-  const updateProgress = React.useCallback(() => {
-    setProgress(Math.random());
-  }, []);
+  const [width] = React.useState(new Animated.Value(0));
+
+  React.useEffect(() => {
+    Animated.timing(width, {
+      toValue: 350,
+      duration: MSEC_IN_FRAME * 350
+    }).start();
+  }, []); // MEMO: Animated使うときは依存でわたさんでも動く
 
   return (
     <View style={styles.container}>
-      <Progress progress={progress} style={styles.progress} color={'red'} />
-      <TouchableOpacity onPress={updateProgress} style={styles.button}>
-        <Text>Update</Text>
-      </TouchableOpacity>
+      <Animated.View style={[styles.block, { width }]} />
     </View>
   );
 }
