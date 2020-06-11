57915ff9f348a8a39f270a5be7cc569362c010f8
和田 充弘 <benzoh.wada@gmail.com>
2020-06-11 20:35:47 +0900

    ## navigationOptionsを動的に変化させる

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { NavigationContainer, useNavigation, DrawerActions } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation, NavigationContainer } from '@react-navigation/native';
 
 const styles = StyleSheet.create({
   container: {
@@ -12,46 +12,50 @@ const styles = StyleSheet.create({
 });
 
 function Main() {
-  const { dispatch } = useNavigation();
+  const { navigate } = useNavigation();
 
   return (
     <View style={styles.container}>
       <Text>Main</Text>
-      <TouchableOpacity onPress={() => dispatch(DrawerActions.openDrawer())}>
-        <Text>open drawer</Text>
+      <TouchableOpacity
+        onPress={() => {
+          navigate('Sub', {
+            title: 'from Main',
+          });
+        }}
+      >
+        <Text>go to sub</Text>
       </TouchableOpacity>
     </View>
   );
 }
 
 function Sub() {
-  const { dispatch } = useNavigation();
-
   return (
     <View style={styles.container}>
       <Text>Sub</Text>
-      <TouchableOpacity onPress={() => dispatch(DrawerActions.openDrawer())}>
-        <Text>open drawer</Text>
-      </TouchableOpacity>
     </View>
   );
 }
 
-const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
-function DrawerNavigator() {
+function StackNavigator() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Main" component={Main} />
-      <Drawer.Screen name="Sub" component={Sub} />
-    </Drawer.Navigator>
+    <Stack.Navigator
+      screenOptions={({ route }) => ({
+        title: route.params && route.params.title,
+      })}>
+      <Stack.Screen name="Main" component={Main} />
+      <Stack.Screen name="Sub" component={Sub} />
+    </Stack.Navigator>
   );
 }
 
 export default function () {
   return (
-    <NavigationContainer>
-      <DrawerNavigator />
+    <NavigationContainer onStateChange={(newState) => console.log(newState)}>
+      <StackNavigator />
     </NavigationContainer>
   );
 }
