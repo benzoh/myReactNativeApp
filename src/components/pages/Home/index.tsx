d6c3add77dbd11e1ac7db70fa5963c4dd7f5b65e
和田 充弘 <benzoh.wada@gmail.com>
2020-06-13 07:47:32 +0900

    ## TabNavigator と StackNavigator を組み合わせる

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { DETAIL } from '../../../constants/path';
 
 const styles = StyleSheet.create({
   container: {
@@ -10,9 +12,14 @@ const styles = StyleSheet.create({
 });
 
 export default function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View style={styles.container}>
       <Text>Home</Text>
+      <TouchableOpacity onPress={() => navigate(DETAIL)}>
+        <Text>Go to Detail</Text>
+      </TouchableOpacity>
     </View>
   );
 }
