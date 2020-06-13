9fa1422117d723ce7a63fa82a2598fc925cda4b2
和田 充弘 <benzoh.wada@gmail.com>
2020-06-13 16:37:54 +0900

    ## 複数のStackNavigatorを使う

--- src/components/pages/ChooseLogin/index.tsx
+++ src/components/pages/ChooseLogin/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { SIGN_IN, SIGN_UP } from '../../../constants/path';
 
 const styles = StyleSheet.create({
   container: {
@@ -10,9 +12,17 @@ const styles = StyleSheet.create({
 });
 
 export default function ChooseLogin() {
+  const { navigate } = useNavigation();
+
   return (
     <View style={styles.container}>
       <Text>Choose Login</Text>
+      <TouchableOpacity onPress={() => navigate(SIGN_IN)}>
+        <Text>go to Sign in</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigate(SIGN_UP)}>
+        <Text>go to Sign up</Text>
+      </TouchableOpacity>
     </View>
   );
 }
