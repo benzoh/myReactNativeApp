e64fa1d646358cbe493aa9a39c2d7e97ceabaee7
和田 充弘 <benzoh.wada@gmail.com>
2020-06-19 06:49:56 +0900

    ## UserInfoの作成

--- src/components/pages/UserInfo/index.tsx
+++ src/components/pages/UserInfo/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { Context, Status } from '../../../contexts/ui';
+import { View, Text, StyleSheet } from 'react-native';
+import { COLOR } from '../../../constants/theme';
+import { Context as UiContext, Status } from '../../../contexts/ui';
+import { Button, Avatar, LabelValueContainer } from '../../atoms';
+import formatDate from '../../../lib/format-date';
 
 const styles = StyleSheet.create({
   container: {
@@ -8,17 +11,48 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  imageIconContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 40,
+  },
+  nameText: {
+    color: COLOR.WHITE,
+    fontSize: 20,
+    marginTop: 5,
+  },
+  button: {
+    marginTop: 5,
+  },
 });
 
+const userState = {
+  name: 'test',
+  createdAt: '2020/01/01',
+  mailAddress: 'test@example.com',
+};
+
 export default function Statistics() {
-  const { setApplicationState } = React.useContext(Context);
+  const { setApplicationState } = React.useContext(UiContext);
+  const signOut = React.useCallback(async () => {
+    setApplicationState(Status.UN_AUTHORIZED);
+  }, [setApplicationState]);
+
+  const source = React.useMemo(() => require('../../../../assets/person.png'), []);
 
   return (
     <View style={styles.container}>
-      <Text>UserInfo</Text>
-      <TouchableOpacity onPress={() => setApplicationState(Status.UN_AUTHORIZED)}>
-        <Text>Sign out</Text>
-      </TouchableOpacity>
+      <View style={styles.imageIconContainer}>
+        <Avatar source={source} />
+        <Text style={styles.nameText}>{userState.name}</Text>
+      </View>
+      <LabelValueContainer label="e-mail" value={userState.mailAddress} />
+      <LabelValueContainer
+        label="registerdAt"
+        value={userState.createdAt && formatDate(new Date(userState.createdAt))}
+      />
+      <Button style={styles.button} onPress={signOut} label="Sign Out" />
     </View>
   );
 }
