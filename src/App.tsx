49351ca008387b4b6f836b722736039ec3e9b36d
和田 充弘 <benzoh.wada@gmail.com>
2020-06-23 21:16:30 +0900

    ## アプリケーションが動作する環境や状況の文脈データ

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,28 @@ import React from 'react';
 import * as UiContext from './contexts/ui';
 import Routes from './routes';
 
+import Home from './components/Home';
+import UserSettingContext, { createInitialContext } from './contexts/user-settings';
+import { loadUserSettings } from '../lib';
+
 export default function App() {
+  const [userSettings, setUserSettings] = React.useState(createInitialContext());
   const [applicationState, setApplicationState] = React.useState(UiContext.createApplicationInitialState());
 
+  React.useEffect(() => {
+    async function load() {
+      const userSettings = await loadUserSettings();
+      setUserSettings(JSON.parse(userSettings));
+    }
+    load();
+  });
+
   return (
-    <UiContext.Context.Provider value={{ applicationState, setApplicationState }}>
-      <Routes />
-    </UiContext.Context.Provider>
+    <UserSettingContext.Provider value={userSettings}>
+      <Home />
+    </UserSettingContext.Provider>
+    // <UiContext.Context.Provider value={{ applicationState, setApplicationState }}>
+    //   <Routes />
+    // </UiContext.Context.Provider>
   );
 }
---
+++ src/contexts/user-settings.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export interface UserSettings {
+  locale: string;
+  theme: string;
+}
+
+export function createInitialContext() {
+  return {
+    locale: 'en-US',
+    theme: 'light',
+  };
+}
+
+export default React.createContext<UserSettings>(createInitialContext());
