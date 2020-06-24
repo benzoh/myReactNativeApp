f401e617c308b4413d1c1614ec5e81c25e21b9af
和田 充弘 <benzoh.wada@gmail.com>
2020-06-24 22:03:25 +0900

    ### その他の振る舞いの定義

--- src/domain/models/todo.ts
+++ src/domain/models/todo.ts
@@ -23,11 +23,37 @@ export function factory(todo: Values): Model {
   const now = new Date().toISOString();
 
   return {
-    id: generateUuid();
+    id: generateUuid(),
     title: todo.title,
     detail: todo.detail,
     createdAt: now,
     updatedAt: now,
     completedAt: null,
+  };
+}
+
+export function toggle(todo: Model): Model {
+  const now = new Date().toISOString();
+
+  return {
+    ...todo,
+    updatedAt: now,
+    completedAt: todo.completedAt === null ? now : null,
+  };
+}
+
+export function isDone(todo: Model): boolean {
+  return todo.completedAt !== null;
+}
+
+export function change(todo: Model, newValues: Values): Model {
+  assertIsDefined(newValues.title);
+
+  const now = new Date().toISOString();
+
+  return {
+    ...todo,
+    ...newValues,
+    updatedAt: now,
   };
 }
---
+++ src/lib/sleep.ts
@@ -0,0 +1,8 @@
+// sleep.ts
+export default function sleep(ms: number) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
