7667e861d641983a73679c2e1580592cdb6d4db1
和田 充弘 <benzoh.wada@gmail.com>
2020-06-28 20:15:35 +0900

    ### 重要なイベント（アプリの設定いじった）

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,52 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import analytics from '@react-native-firebase/analytics';
+import { NavigationState, NavigationContainer } from '@react-navigation/native';
+import { PartialState } from '@react-navigation/routers';
+
 import MainRoutes from './Main';
 
+function isNavigationState(state: NavigationState | PartialState<NavigationState>): state is NavigationState {
+  return 'index' in state;
+}
+
+const getActiveRouteName = (state: NavigationState): string => {
+  const route = state.routes[state.index];
+
+  const { state: childState } = route;
+  if (childState && isNavigationState(childState)) {
+    // Dive into nested navigators
+    return getActiveRouteName(childState);
+  }
+
+  return route.name;
+}
+
+function isDefined(state: NavigationState | undefined): state is NavigationState {
+  return state !== undefined;
+}
+
+const onNavigationStateChange = (routeNameRef: React.MutableRefObject<undefined | string>) => (
+  prevState: NavigationState | undefined,
+) => {
+  if (!isDefined(prevState)) {
+    return;
+  }
+
+  const previousRouteName = routeNameRef.current;
+  const currentRouteName = getActiveRouteName(prevState);
+
+  if (previousRouteName !== currentRouteName) {
+    analytics().setCurrentScreen(currentRouteName, currentRouteName);
+  }
+
+  routeNameRef.current = currentRouteName;
+};
+
 export default function LoggingRoutes() {
+  const routeNameRef = React.useRef();
+
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={routeNameRef} onStateChange={onNavigationStateChange(routeNameRef)}>
       <MainRoutes />
     </NavigationContainer>
   );
